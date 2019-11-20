e7b1fa4e7d70971575e15072470acf0b1ef09f89
Yuya Sato <yuya.sato.verygood@gmail.com>
2019-11-20 13:39:54 +0900

    add docker-lamp-light

---
+++ html/index.php
@@ -0,0 +1,9 @@
+<?php
+
+
+try {
+    $pdo = new PDO('mysql:host=mysql;dbname=test;charset=utf8mb4','root','pass');
+} catch (PDOException $e) {
+    exit($e->getMessage().PHP_EOL);
+}
+echo('ok'.PHP_EOL);
